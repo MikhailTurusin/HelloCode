36243a2bde8813431386c3381936e95054add4cb
MikhailT <m.tyrysin@gmail.com>
2023-02-13 21:21:29 +0300

    Added Exp5 IfElse

---
+++ Exp5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовател: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура, это Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
